086b0e370cf0f288d3a165e82bf828f885e4854f
Silas Scholz <30719399+Drandalf@users.noreply.github.com>
2024-09-06 20:19:05 +0200

    added dynamic images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NgOptimizedImage, NgFor, NgClass, NgIf} from "@angular/common";
+import {NgOptimizedImage, NgFor, NgClass, NgIf, NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage, NgFor, NgClass, NgIf],
+  imports: [RouterOutlet, NgOptimizedImage, NgFor, NgClass, NgIf, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dalliClick';
+
+
   // Define the grid pieces state
   gridPieces = Array.from({ length: 12 }, () => ({ hidden: false }));
+
+  // Array to hold image objects
+  images: { index: number; path: string }[] = [];
+
+  count = 1;
+
+  constructor() {
+    // Initialize images array
+    this.loadImages();
+  }
+
+  // Function to initialize images array
+  loadImages(): void {
+    const imagePaths = [
+      'assets/images/image_1.jpeg',
+      'assets/images/image_2.jpg',
+    ];
+
+    this.images = imagePaths.map((path, index) => ({ index, path }));
+  }
+
+
 
   // Function to reveal a piece
   revealPiece(index: number): void {
